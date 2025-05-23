f3836d83e866d6c39cb438ff15f983115c67ecc5
Pratyush Pratik Sinha <pratyushpratiksinha@gmail.com>
2025-05-23 23:36:28 +0530

    feat: add home screen with MVVM structure

---
+++ iOSApp-VibeCodingTest/Features/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  iOSApp-VibeCodingTest
+//
+//  Created by Pratyush Pratik Sinha on 23/05/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    HomeView()
+}
