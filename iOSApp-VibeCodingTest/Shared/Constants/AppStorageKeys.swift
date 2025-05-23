e58c2c72d2da305965240b52feae7058d3391e4a
Pratyush Pratik Sinha <pratyushpratiksinha@gmail.com>
2025-05-23 23:16:27 +0530

    feat: create constants AppStorageKeys

---
+++ iOSApp-VibeCodingTest/Shared/Constants/AppStorageKeys.swift
@@ -0,0 +1,12 @@
+//
+//  AppStorageKeys.swift
+//  iOSApp-VibeCodingTest
+//
+//  Created by Pratyush Pratik Sinha on 23/05/25.
+//
+
+import Foundation
+
+enum AppStorageKeys {
+    static let username = "username"
+}
