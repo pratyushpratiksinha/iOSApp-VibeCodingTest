696474dc04b2fd13908a1a2d9bca8a4b8a257ab8
Pratyush Pratik Sinha <pratyushpratiksinha@gmail.com>
2025-05-24 11:17:20 +0530

    feat: add CircularProgressBar

---
+++ iOSApp-VibeCodingTest/Shared/Components/CircularProgressBar.swift
@@ -0,0 +1,28 @@
+//
+//  CircularProgressBar.swift
+//  iOSApp-VibeCodingTest
+//
+//  Created by Pratyush Pratik Sinha on 24/05/25.
+//
+
+import SwiftUI
+
+struct CircularProgressBar: View {
+    let progress: Double
+    let image: String
+    let color: Color
+    let lineWidth: CGFloat
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(Color.gray.opacity(0.2), lineWidth: lineWidth)
+            Circle()
+                .trim(from: 0, to: progress)
+                .stroke(color, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
+                .rotationEffect(.degrees(-90))
+            Image(systemName: image)
+                .foregroundColor(color)
+        }
+    }
+}
