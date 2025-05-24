c47a537ee9ca135068c8552ecf6cbd8d59752363
Pratyush Pratik Sinha <pratyushpratiksinha@gmail.com>
2025-05-24 11:09:13 +0530

    feat: add Date extension with helper utilities

--- iOSApp-VibeCodingTest/Shared/Extensions/Date+Extension.swift
+++ iOSApp-VibeCodingTest/Shared/Extensions/Date+Extension.swift
@@ -1,8 +1,14 @@
 //
-//  Date+Format.swift
+//  Date+Extension.swift
 //  iOSApp-VibeCodingTest
 //
-//  Created by Pratyush Pratik Sinha on 23/05/25.
+//  Created by Pratyush Pratik Sinha on 24/05/25.
 //
 
 import Foundation
+
+extension Date {
+    var onlyDate: Date {
+        Calendar.current.startOfDay(for: self)
+    }
+}
