17b4c64345cde60ab3f0a856d61b2aa2b2dc19fd
Pratyush Pratik Sinha <pratyushpratiksinha@gmail.com>
2025-05-24 11:17:02 +0530

    feat: add FloatingActionButton

---
+++ iOSApp-VibeCodingTest/Shared/Components/FloatingActionButton.swift
@@ -0,0 +1,27 @@
+//
+//  FloatingActionButton.swift
+//  iOSApp-VibeCodingTest
+//
+//  Created by Pratyush Pratik Sinha on 24/05/25.
+//
+
+import SwiftUI
+
+struct FloatingActionButton: View {
+    let action: () -> Void
+    var body: some View {
+        Button(action: action) {
+            Image(systemName: Constants.plusIcon)
+                .font(.system(size: 28, weight: .bold))
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.accentColor)
+                .clipShape(Circle())
+                .shadow(radius: 4)
+        }
+    }
+    
+    private enum Constants {
+        static let plusIcon = "plus"
+    }
+}
